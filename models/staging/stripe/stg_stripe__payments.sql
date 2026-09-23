23eea02c34538b1639e849e96671e98bc02e940e
youssef trabelsi <youssef-khaled.trabelsi@capgemini.com>
2026-09-23 16:02:38 +0000

    adding source file to jaffle_shop folder + updating staging customers and orders sql files

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,10 @@
+select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status as payment_status,
+        amount as payment_amount,
+        created as payment_created,
+        _batched_at
+
+    from raw.stripe.payments
